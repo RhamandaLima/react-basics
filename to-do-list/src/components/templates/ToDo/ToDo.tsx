37f1a3c2431bbc28ef8dfa061dc1bff4dd36f968
Rhamanda Lima <cathyannarhamanda@gmail.com>
2025-04-25 19:37:46 -0300

    feat: create todo component

---
+++ to-do-list/src/components/templates/ToDo/ToDo.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import styles from './ToDo.module.css';
+
+import { NewTask } from '../../molecules/NewTask/NewTask';
+import { TaskBoard } from '../../organisms/TaskBoard/TaskBoard';
+import { TaskInfo } from '../../../shared/interfaces/task.interface';
+
+export function ToDo() {
+  const [tasks, setTasks] = useState<TaskInfo[]>([]);
+
+  function handleCreateTask(newTask: TaskInfo): void {
+    setTasks((prev: TaskInfo[]) => [...prev, newTask]);
+  }
+
+  function handleToggleChecked(taskId: string, checked: boolean): void {
+    setTasks((prev: TaskInfo[]) =>
+      prev.map((task: TaskInfo) =>
+        task.id === taskId ? { ...task, checked } : task
+      )
+    );
+  }
+
+  function handleDeleteTask(taskId: string): void {
+    setTasks((prev) => prev.filter((task) => task.id !== taskId));
+  }
+
+  return (
+    <div className={styles.todo}>
+      <div style={{ marginBottom: '3.313rem' }}>
+        <NewTask onCreate={handleCreateTask} />
+      </div>
+      <TaskBoard
+        taskList={tasks}
+        onCheckboxChange={handleToggleChecked}
+        onTaskDelete={handleDeleteTask}
+      />
+    </div>
+  );
+}
